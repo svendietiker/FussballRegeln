5ddc39d74d401b8d3866ae29bf287383d8917d04
svendietiker <sven.dietiker@gmail.com>
2026-05-12 16:52:17 +0200

    add

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,14 +1,14 @@
 import { MongoClient } from 'mongodb';
-import { MONGODB_URI, MONGODB_DB } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
-const client = new MongoClient(MONGODB_URI);
+const client = new MongoClient(env.MONGODB_URI);
 
 let db;
 
 export async function connectToDatabase() {
 	if (!db) {
 		await client.connect();
-		db = client.db(MONGODB_DB);
+		db = client.db('fussballregeln');
 	}
 
 	return db;
