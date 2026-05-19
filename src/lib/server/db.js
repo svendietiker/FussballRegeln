83c426507e3f87a980283939e4dff4c9e89e2201
svendietiker <sven.dietiker@gmail.com>
2026-05-19 22:14:01 +0200

    new

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,15 +1,22 @@
 import { MongoClient } from 'mongodb';
 import { env } from '$env/dynamic/private';
 
-const client = new MongoClient(env.MONGODB_URI);
+const uri = env.MONGODB_URI;
+const dbName = env.MONGODB_DB || 'fussballregeln';
 
+if (!uri) {
+	throw new Error('MONGODB_URI fehlt in Netlify Environment Variables');
+}
+
+let client;
 let db;
 
 export async function connectToDatabase() {
-	if (!db) {
-		await client.connect();
-		db = client.db('fussballregeln');
-	}
+	if (db) return db;
 
+	client = new MongoClient(uri);
+	await client.connect();
+
+	db = client.db(dbName);
 	return db;
 }
